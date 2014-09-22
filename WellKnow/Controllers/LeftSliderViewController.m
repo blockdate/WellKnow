f2330aeca38bd26c7a1b687aa4beea4805f600ac
block <869938879@qq.com>
2014-09-22 22:41:25 +0800

    1

---
+++ WellKnow/Controllers/LeftSliderViewController.m
@@ -0,0 +1,49 @@
+//
+//  LeftSliderViewController.m
+//  WellKnow
+//
+//  Created by block on 14-9-22.
+//  Copyright (c) 2014年 Block. All rights reserved.
+//
+
+#import "LeftSliderViewController.h"
+
+@interface LeftSliderViewController ()
+
+@end
+
+@implementation LeftSliderViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
--- WellKnow/Views/WL/Cell/WeatherInfoCell.m
+++ WellKnow/Views/WL/Cell/WeatherInfoCell.m
@@ -70,8 +70,8 @@
 
 - (void)f{
     NSMutableArray *imageArray = [NSMutableArray array];
-    for (NSInteger i = 7; i<=8; i++) {
-//    for (NSInteger i = 14; i<=17; i++) {
+//    for (NSInteger i = 7; i<=8; i++) {
+    for (NSInteger i = 14; i<=17; i++) {
         UIImage *image = [UIImage imageNamed:[NSString stringWithFormat:@"w%ld",i]];
         [imageArray addObject:image];
     }
